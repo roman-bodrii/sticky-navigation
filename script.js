fb93dbd8f1f6c43e560e7df806a0f15456757d8c
Roman Bodrii <roman@bodrii.ru>
2021-10-19 10:27:34 +0300

    it's work !!!

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',function (){
+    let navbar = document.getElementById('navbar');
+    let sticky = navbar.offsetTop;
+
+    window.onscroll = function ()
+    {
+        if(window.pageYOffset >= sticky ){
+            navbar.classList.add('sticky');
+        }else{
+            navbar.classList.remove('sticky');
+        }
+    }
+});
